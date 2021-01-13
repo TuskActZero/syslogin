dc4cefbbf2fcb84677d530f73f8033c966a7f9d3
carlos <pikmam@gmail.com>
2021-01-13 20:50:52 -0300

    database8

--- login/settings.py
+++ login/settings.py
@@ -79,21 +79,15 @@ WSGI_APPLICATION = 'login.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 import dj_database_url
 from decouple import config
 
-#DATABASE = {
-#   'default': dj_database_url.config(
-#        default=config('DATABASE_URL')
-#   )
-#}
+DATABASE = {
+   'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+   )
+}
 
 #DATABASES = {
 #    'default': {
@@ -103,9 +97,6 @@ from decouple import config
 #}
 
 
-db_config = dj_database_url.config()
-if db_config:
-    DATABASES['default'] =  db_config
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
