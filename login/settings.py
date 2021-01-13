25a6e2e7b1c1723fdb2fd5a895f7f88090d37efd
carlos <pikmam@gmail.com>
2021-01-13 20:29:25 -0300

    database6.1

--- login/settings.py
+++ login/settings.py
@@ -92,11 +92,11 @@ from decouple import config
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
-#DATABASE = {
-#   'default': dj_database_url.config(
-#        default=config('DATABASE_URL')
-#   )
-#}
+DATABASE = {
+   'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+   )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
