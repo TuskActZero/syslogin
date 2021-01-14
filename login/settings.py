511a68320d7ca5a4a1700388399e414ac4845bed
carlos <pikmam@gmail.com>
2021-01-13 21:21:38 -0300

    database10.4

--- login/settings.py
+++ login/settings.py
@@ -84,13 +84,10 @@ WSGI_APPLICATION = 'login.wsgi.application'
 import dj_database_url
 from decouple import config
 
-
-
-
-DATABASE = {
-   'default': dj_database_url.config(
+DATABASES = {
+    'default': dj_database_url.config(
         default=config('DATABASE_URL')
-   )
+    )
 }
 
 #DATABASES = {
