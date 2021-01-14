2a57def73695cc485ea11feed1653cc0b8074176
carlos <pikmam@gmail.com>
2021-01-13 21:14:14 -0300

    database10.3

--- login/settings.py
+++ login/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,4 +155,3 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
